4f533d1967ecd4c9dd0f89271878c8a92e4cde06
wvxbs <gabriel.ferreira7854@gmail.com>
2021-08-13 11:52:17 -0300

    .

---
+++ aula 002/ex4.py
@@ -0,0 +1,5 @@
+numeroSemQuaradado = int(input("digite o número e para calcular seu quadrado: "))
+
+numeroComQuadrado = numeroSemQuaradado ** 2
+
+print("O quadrado do Número é:", numeroComQuadrado)
--- ex4.py
+++
@@ -1,5 +0,0 @@
-numeroSemQuaradado = int(input("digite o número e mostre o quadrado dele: "))
-
-numeroComQuadrado = numeroComQuadrado ** 2
-
-print("O quadrado do Número é:", numeroComQuadrado)
