f6a3e37e0bd8c360ddce34701b9949b8233dd40a
wvxbs <gabriel.ferreira7854@gmail.com>
2021-08-13 11:44:51 -0300

    first commit

---
+++ ex2.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite o primeiro número: "))
+num2 = int(input("digite o segundo número: "))
+num3 = int(input("digite o terceiro número: "))
+num4 = int(input("digite o quarto número: "))
+
+soma =num1 + num2 + num3 + num4
+
+print("O resultado da soma é", soma)
---
+++ ex3.py
@@ -0,0 +1,5 @@
+salario = float(input("Digite aqui seu salário: "))
+
+salariofinal = salario * 1.25
+
+print ("Salario final: ", salariofinal)
