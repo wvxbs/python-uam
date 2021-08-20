ec435aba159142f2176a75df4cff95bfe8fa8fd5
wvxbs <gabriel.ferreira7854@gmail.com>
2021-08-20 10:41:40 -0300

    exercise 1 and 2 added

---
+++ aula 003/exercicios/Exe1.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número inteiro"))
+metade = 0
+
+if numero > 20:
+    metade = numero/2
+    print(metade)
+else:
+    print(numero)
---
+++ aula 003/exercicios/Exe2.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um numero inteiro"))
+
+if numero > 49 and numero < 101:
+    print(numero,"prentence ao intervalo")
+
+else:
+    print(numero,"não pertence ao intervalo")
